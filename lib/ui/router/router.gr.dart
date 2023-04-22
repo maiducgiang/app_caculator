ff016db638bdfd0fd27907c8995f109f8b0e7b96
TamHM <minhtamhoang207@gmail.com>
2023-04-22 22:56:41 +0700

    add drawer menu

--- lib/ui/router/router.gr.dart
+++ lib/ui/router/router.gr.dart
@@ -11,59 +11,81 @@
 // ignore_for_file: type=lint
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i4;
-import 'package:flutter/material.dart' as _i5;
+import 'package:auto_route/auto_route.dart' as _i6;
+import 'package:flutter/material.dart' as _i7;
 
+import '../screen/calculator/calculator_screen.dart' as _i4;
+import '../screen/choose_theme/choose_theme_screen.dart' as _i5;
 import '../screen/main/main_screen.dart' as _i2;
 import '../screen/sign_up/sign_up_screen.dart' as _i3;
 import '../screen/splash/splash_screen.dart' as _i1;
 
-class AppRouter extends _i4.RootStackRouter {
-  AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
+class AppRouter extends _i6.RootStackRouter {
+  AppRouter([_i7.GlobalKey<_i7.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i4.PageFactory> pagesMap = {
+  final Map<String, _i6.PageFactory> pagesMap = {
     SplashPage.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i6.MaterialPageX<dynamic>(
         routeData: routeData,
         child: const _i1.SplashScreen(),
       );
     },
     MainPage.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i6.MaterialPageX<dynamic>(
         routeData: routeData,
         child: const _i2.MainScreen(),
       );
     },
     SignUp.name: (routeData) {
-      return _i4.MaterialPageX<dynamic>(
+      return _i6.MaterialPageX<dynamic>(
         routeData: routeData,
         child: const _i3.SignUpScreen(),
+      );
+    },
+    Calculator.name: (routeData) {
+      return _i6.MaterialPageX<dynamic>(
+        routeData: routeData,
+        child: const _i4.CalculatorScreen(),
+      );
+    },
+    ChooseTheme.name: (routeData) {
+      return _i6.MaterialPageX<dynamic>(
+        routeData: routeData,
+        child: const _i5.ChooseThemeScreen(),
       );
     },
   };
 
   @override
-  List<_i4.RouteConfig> get routes => [
-        _i4.RouteConfig(
+  List<_i6.RouteConfig> get routes => [
+        _i6.RouteConfig(
           SplashPage.name,
           path: '/',
         ),
-        _i4.RouteConfig(
+        _i6.RouteConfig(
           MainPage.name,
           path: '/main',
         ),
-        _i4.RouteConfig(
+        _i6.RouteConfig(
           SignUp.name,
           path: '/signup_screen',
+        ),
+        _i6.RouteConfig(
+          Calculator.name,
+          path: '/calculator',
+        ),
+        _i6.RouteConfig(
+          ChooseTheme.name,
+          path: '/choose_theme',
         ),
       ];
 }
 
 /// generated route for
 /// [_i1.SplashScreen]
-class SplashPage extends _i4.PageRouteInfo<void> {
+class SplashPage extends _i6.PageRouteInfo<void> {
   const SplashPage()
       : super(
           SplashPage.name,
@@ -75,7 +97,7 @@ class SplashPage extends _i4.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.MainScreen]
-class MainPage extends _i4.PageRouteInfo<void> {
+class MainPage extends _i6.PageRouteInfo<void> {
   const MainPage()
       : super(
           MainPage.name,
@@ -87,7 +109,7 @@ class MainPage extends _i4.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.SignUpScreen]
-class SignUp extends _i4.PageRouteInfo<void> {
+class SignUp extends _i6.PageRouteInfo<void> {
   const SignUp()
       : super(
           SignUp.name,
@@ -95,4 +117,28 @@ class SignUp extends _i4.PageRouteInfo<void> {
         );
 
   static const String name = 'SignUp';
+}
+
+/// generated route for
+/// [_i4.CalculatorScreen]
+class Calculator extends _i6.PageRouteInfo<void> {
+  const Calculator()
+      : super(
+          Calculator.name,
+          path: '/calculator',
+        );
+
+  static const String name = 'Calculator';
+}
+
+/// generated route for
+/// [_i5.ChooseThemeScreen]
+class ChooseTheme extends _i6.PageRouteInfo<void> {
+  const ChooseTheme()
+      : super(
+          ChooseTheme.name,
+          path: '/choose_theme',
+        );
+
+  static const String name = 'ChooseTheme';
 }
--- lib/ui/screen/main/main_screen.dart
+++ lib/ui/screen/main/main_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mubaha/ui/screen/main/cubit/main_cubit.dart';
-import 'package:mubaha/ui/screen/main/cubit/main_state.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mubaha/icons/assets.gen.dart';
+import 'package:mubaha/ui/screen/calculator/calculator_screen.dart';
+import 'package:mubaha/ui/screen/choose_theme/choose_theme_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -11,20 +12,111 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    const CalculatorScreen(),
+    Container(color: Colors.blue),
+    const ChooseThemeScreen()
+  ];
+
+  final List<String> _title = [
+    'Máy tính',
+    'Chuyển đổi tiền tệ',
+    'Chủ đề',
+    'Đăng xuất'
+  ];
+
+  final List<String> _icon = [
+    Assets.images.icCalculator.path,
+    Assets.images.icCurrencyConvert.path,
+    Assets.images.icTheme.path,
+    Assets.images.icLogout.path
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    Navigator.pop(context);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => MainCubit(),
-      child: BlocConsumer<MainCubit, MainState>(
-        listener: (context, state) {
-          // TODO: implement listener
-        },
-        builder: (context, state) {
-          return Scaffold(
-            body: Container(),
-          );
-        },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          _title[_selectedIndex],
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+        ),
+        centerTitle: false,
       ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(
+              height: 112.h,
+              child: DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFF6B6B),
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(20)
+                  )
+                ),
+                child: Row(
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, color: Colors.grey),
+                    ),
+                    SizedBox(width: 16.w),
+                    const Flexible(
+                      child: Text(
+                          'Xin chào, Tom',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                          color: Colors.white
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ),
+            ),
+
+            Column(
+              children: List.generate(4, (index){
+                return ListTile(
+              leading: Image.asset(
+                _icon[index],
+                height: 24,
+                width: 24,
+              ),
+              title: Text(_title[index]),
+              selected: _selectedIndex == index,
+              onTap: () {
+                if(index == 3) {
+                  ///TODO: implement logout
+                } else {
+                  _onItemTapped(index);
+                }
+              },
+            );
+              }),
+            )
+          ],
+        ),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex)
     );
   }
 }
