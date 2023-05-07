ea78ffe76b2f41255a68bd1cef62e16653fd4b2f
Mabook Pro <>
2023-05-07 20:46:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mubaha/data/cache_manager.dart';
+import 'package:mubaha/translations/codegen_loader.g.dart';
 import 'package:mubaha/ui/services/ThemePreferences.dart';
 import 'package:sp_util/sp_util.dart';
 import 'app.dart';
@@ -13,7 +14,6 @@ void main() async {
   await Firebase.initializeApp();
   await ThemePreferences.init();
   await EasyLocalization.ensureInitialized();
-  //final langugage = await AccountServices().getAccountLanguage();
   await CacheManager.instance.init();
   //await dotenv.load(fileName: ".env");
   await SpUtil.getInstance();
@@ -22,6 +22,6 @@ void main() async {
       supportedLocales: const [Locale("en"), Locale("vi")],
       fallbackLocale: const Locale("en"),
       startLocale: Locale("vi"),
-      assetLoader: Container(),
+      assetLoader: CodegenLoader(),
       child: MyApp()));
 }
